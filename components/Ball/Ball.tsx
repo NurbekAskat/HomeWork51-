a9f5c49167855fb824801ffc318b4071e71c1291
NurbekAskat <Nurbeka738@gmail.com>
2024-05-22 15:12:39 +0600

    основная часть

---
+++ components/Ball/Ball.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface BallProps {
+  number: number;
+}
+
+const Ball: React.FC<BallProps> = ({ number }) => {
+  return (
+    <div className="ball">
+      {number}
+    </div>
+  );
+};
+
+export default Ball;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import './App.css'
+import './App.css';
+import {useState} from 'react';
+import Ball from '../components/Ball/Ball';
 
 const App = () => {
+  const initialNumbers = [5, 11, 16, 23, 32];
+  const [numbers, setNumbers] = useState<number[]>(initialNumbers);
 
- return (
-    <>
+  const generateNewNumbers = () => {
+    const newNumbers: number[] = [];
+    while (newNumbers.length < 5) {
+      const rand = Math.floor(Math.random() * 32) + 5;
+      if (!newNumbers.includes(rand)) {
+        newNumbers.push(rand);
+      }
+    }
+    newNumbers.sort((a, b) => a - b);
+    setNumbers(newNumbers);
+  };
 
-    </>
-  )
+  return (
+    <div className="app">
+      <h1>Lottery Draw</h1>
+      <div className="numbers">
+        {numbers.map(number => (
+          <Ball key={number} number={number}/>
+        ))}
+      </div>
+      <button onClick={generateNewNumbers}>New numbers</button>
+    </div>
+  );
 };
 
-export default App
+export default App;
